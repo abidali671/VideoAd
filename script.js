7921bb58d94cde7a3548b79e29390c08f97f82ff
abidali671 <abid.saeed.ali92@gmail.com>
2020-11-21 01:33:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Redirect Link
+let redirectLocation =
+  "https://www.brainpickings.org/?fbclid=IwAR2PMDwZzDngDNZiWa7bKswWXvQfsREiRy12a67KfHhsSOBi6y-w2yEpXao";
+// Video Link
+let videoLink = "./video-1605777762.mp4";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Video Script
+let videoPlayer = document.querySelector("#starter-video");
+let skipBtn = document.querySelector("#skip-btn");
+let muteBtn = document.querySelector("#mute-btn");
+videoPlayer.setAttribute("src", videoLink);
+videoPlayer.muted = true;
+
+// let elem = document.querySelector("#starter-video");
+// videoPlayer.addEventListener("mouseenter", function () {
+//   if (!document.fullscreenElement) {
+//     elem.requestFullscreen().catch((err) => {
+//       alert(
+//         `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+//       );
+//     });
+//   } else {
+//     document.exit;
+//   }
+// });
+videoPlayer.play();
+videoPlayer.onended = function () {
+  // window.location.replace(redirectLocation);
+};
+skipBtn.addEventListener("click", function () {
+  window.location.replace(redirectLocation);
+});
+muteBtn.addEventListener("click", function () {
+  if (videoPlayer.muted == true) {
+    muteBtn.innerHTML = `<i class="fas fa-volume-up"></i>`;
+    videoPlayer.muted = false;
+  } else if (videoPlayer.muted == false) {
+    muteBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
+    videoPlayer.muted = true;
+  }
+});
